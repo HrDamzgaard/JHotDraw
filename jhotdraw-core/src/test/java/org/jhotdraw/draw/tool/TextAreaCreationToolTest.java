a0842ac8013aa95ed1a9a325fe7ba5be53389b82
Matt-G-K <mataeosp@gmail.com>
2026-02-19 16:26:32 +0100

    Unit test for creation tool

--- jhotdraw-core/src/test/java/org/jhotdraw/draw/tool/TextAreaCreationToolTest.java
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/tool/TextAreaCreationToolTest.java
@@ -1,9 +1,90 @@
 package org.jhotdraw.draw.tool;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.jhotdraw.draw.Drawing;
+import org.jhotdraw.draw.DrawingEditor;
+import org.jhotdraw.draw.DrawingView;
+import org.jhotdraw.draw.figure.TextHolderFigure;
+import org.jhotdraw.draw.text.FloatingTextArea;
 import org.junit.Before;
 import org.junit.Test;
+
+import javax.swing.undo.UndoableEdit;
+import java.awt.geom.Rectangle2D;
+
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
 public class TextAreaCreationToolTest {
+    private TextHolderFigure typingTarget;
+    private FloatingTextArea textArea;
+    private DrawingEditor drawingEditor;
+    private TextAreaEditingTool textAreaEditingTool;
+    private TextUndoableEdit textUndoableEdit;
+    private Drawing drawing;
+    private DrawingView drawingView;
+    private TextAreaFieldBounds textAreaFieldBounds;
+
+    @Before
+    public void setUp() {
+        typingTarget = mock(TextHolderFigure.class);
+        textArea = mock(FloatingTextArea.class);
+        drawingEditor = mock(DrawingEditor.class);
+        drawing = mock(Drawing.class);
+        drawingView = mock(DrawingView.class);
+        textUndoableEdit = mock(TextUndoableEdit.class);
+        textAreaFieldBounds = mock(TextAreaFieldBounds.class);
+
+        textAreaEditingTool = new TextAreaEditingTool(typingTarget);
+
+        UndoableEdit undoableEdit = mock(UndoableEdit.class);
+
+        when(textUndoableEdit.makeUndoableEdit(any(), anyString(), anyString())).thenReturn(undoableEdit);
+        when(typingTarget.getText()).thenReturn("original text");
+        when(textArea.getText()).thenReturn("changed text");
+        when(drawingEditor.getActiveView()).thenReturn(drawingView);
+        when(drawingView.getDrawing()).thenReturn((drawing));
+        when(drawingView.getComponent()).thenReturn(new javax.swing.JPanel());
+        when(textAreaFieldBounds.getRectangleFieldBounds(typingTarget)).thenReturn(new Rectangle2D.Double());
+        doNothing().when(textArea).endOverlay();
+    }
+
+    @Test
+    public void testEndEdit() throws IllegalAccessException {
+        textUndoableEdit = new TextUndoableEdit();
+
+        FieldUtils.writeField(textAreaEditingTool, "editor", drawingEditor, true);
+        FieldUtils.writeField(textAreaEditingTool, "textArea", textArea, true);
+        FieldUtils.writeField(textAreaEditingTool, "textUndoableEdit", textUndoableEdit, true);
+        FieldUtils.writeField(textAreaEditingTool, "typingTarget", typingTarget, true);
+
+        textAreaEditingTool.endEdit();
+
+        Object valueOfField = FieldUtils.readField(textAreaEditingTool, "typingTarget", true);
+
+        assertNull(valueOfField);
+        verify(typingTarget).setText("changed text");
+        verify(drawing).fireUndoableEditHappened(any());
+        verify(textArea).endOverlay();
+    }
+
+    @Test
+    public void testBeginEdit() throws IllegalAccessException {
+        textUndoableEdit = new TextUndoableEdit();
+
+        FieldUtils.writeField(textAreaEditingTool, "editor", drawingEditor, true);
+        FieldUtils.writeField(textAreaEditingTool, "textArea", textArea, true);
+        FieldUtils.writeField(textAreaEditingTool, "textUndoableEdit", textUndoableEdit, true);
+        FieldUtils.writeField(textAreaEditingTool, "textAreaFieldBounds", textAreaFieldBounds, true);
+        FieldUtils.writeField(textAreaEditingTool, "typingTarget", null, true);
+
+        textAreaEditingTool.beginEdit(typingTarget);
+
+        Object valueOfField = FieldUtils.readField(textAreaEditingTool, "typingTarget", true);
+
+        assertEquals(typingTarget, valueOfField);
+        verify(textArea).createOverlay(drawingView, typingTarget);
+        verify(textArea).setBounds(any(), eq("original text"));
+        verify(textArea).requestFocus();
+    }
 }
